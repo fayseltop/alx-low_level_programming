eceeaeb07df4ef997dea3c155bc5db5965c96a82
fayseltop <fayselshemsu2014@gmail.com>
2022-07-03 07:18:09 -0700

    hot fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
-main - prints exactly "and that piece of art is useful"
-- Dora Korpar, 2015-10-19",
-followed by a new line, to the standard error.
-Return: Always 0 (Success)
+* main - prints the size of various types
+* Return: 0 if exited properly, non-zero otherwise
 */
 int main(void)
 {
