daa23f006556b3a909432c2123ec640cf0bd7c90
fayseltop <fayselshemsu2014@gmail.com>
2022-07-04 13:18:22 -0700

    h

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - prints the size of various types
+* Return: 0 if exited properly, non-zero otherwise
+*/
+int main(void)
+{
+int ch;
+for (ch = 48; ch <= 57; ch++)
+{
+putchar(ch);
+}
+putchar(10);
+return (0);
+}
