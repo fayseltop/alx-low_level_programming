edd1169c28baaa91526b3f36b70136cb9dd58167
fayseltop <fayselshemsu2014@gmail.com>
2022-07-04 04:47:19 -0700

    h

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,11 +17,11 @@ printf("and is greater than 5");
 }
 if (n == 0)
 {
-printf("0");
+printf("and is 0");
 }
 if (n < 6 && n != 0)
 {
-printf("less than 6 and not 0");
+printf("and is less than 6 and not 0");
 }
 printf("\n");
 return (0);
