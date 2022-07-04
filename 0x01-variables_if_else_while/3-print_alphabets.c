12d0fa5fc49cf5ee31a611e211859a46fbc9d5c9
fayseltop <fayselshemsu2014@gmail.com>
2022-07-04 12:19:07 -0700

    h

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - prints the size of various types
+* Return: 0 if exited properly, non-zero otherwise
+*/
+int main(void)
+{
+int ch;
+for (ch = 97; ch <= 122; ch++)
+{
+putchar(ch);
+}
+for (ch = 65; ch <= 90; ch++)
+{
+putchar(ch);
+}
+putchar(10);
+return (0);
+}
