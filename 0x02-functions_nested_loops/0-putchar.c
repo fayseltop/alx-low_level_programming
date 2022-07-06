4f235baa0f8e8c9317d5ab9888ef0bc06437ba83
fayseltop <fayselshemsu2014@gmail.com>
2022-07-06 12:54:29 -0700

    h

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,17 @@
 #include "main.h"
-/** main - prints putchar
-*Return: always 0
+/**
+* main - prints the size of various types
+* Return: 0 if exited properly, non-zero otherwise
 */
 int main(void)
 {
-        _putchar("_") ;
-        _putchar("p") ;
-        _putchar("u") ;
-        _putchar("t") ;
-        _putchar("c") ;
-        _putchar("h") ;
-        _putchar("a") ;
-        _putchar("r") ;
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 return (0);
-ii}
+}
