0a71b753dd17e77518827d8126402bdf114071b4
fayseltop <fayselshemsu2014@gmail.com>
2022-07-08 12:35:55 -0700

    h

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,11 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
+* _isupper -> this is a function to check upper case alphas
+* @c: an integer paramer
+* Return: something but success
+*/
+int _isupper(int c)
 {
-	return (write(1, &c, 1));
+	return (c >= 'A' && c <= 'Z');
 }
