5c28bcbee31066dff483aa756c917266bf44bff6
fayseltop <fayselshemsu2014@gmail.com>
2022-07-11 13:13:56 -0700

    ho

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * swap_int - gitvfe two integer
+ * @a: parame;ter 1.
+ * @b: parameter 2.
+ */
+void swap_int(int *a, int *b)
+{
+int tmp;
+tmp = *a;
+*a = *b;
+*b = tmp;
+}
