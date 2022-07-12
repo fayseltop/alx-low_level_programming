6cb3962c9f4ac6112068528d0234d4e4362ca856
fayseltop <fayselshemsu2014@gmail.com>
2022-07-11 21:32:15 -0700

    hoo

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+
+/**
+*  _strlen -> function to get the length of string.
+* @s: string pointre to passed to this function.
+*Return: rreturn length of the string.
+*/
+int _strlen(char *s)
+{
+return (strlen(s));
+}
